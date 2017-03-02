6620ec0cd7f13d1807cf68ca41a2634adc766d06
Thisita <Thisita@users.noreply.github.com>
2017-03-01 19:50:26 -0500

    [prototype] rest of express application init using controller bootstrap and session support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import session from 'express-session';
 import bodyParser from 'body-parser';
 import methodOverride from 'method-override';
 
@@ -7,17 +8,91 @@ import methodOverride from 'method-override';
 import './models';
 
 // Create express app
-
 let app = express();
+// Expose it
+export default app;
 
+// Express settings
+app.set('view engine', 'jade');
+
+// Set views for error and 404 pages
+app.set('views', __dirname + '/views');
+
+// Define a custom res.message() method
+// which stores messages in the session
+app.response.message = function(msg) {
+  // Reference 'req.session' via the 'this.req' reference
+  let sess = this.req.session;
+  // Simply add the msg to an array for later
+  sess.messages = sess.messages || [];
+  sess.messages.push(msg);
+  return this;
+}
+
+// Set up morgan logging
+if(!module.parent) {
+  app.use(morgan('dev'));
+}
+
+// Serve static files
 app.use(express.static(__dirname + '/public'));
+
+// Serve static bootstrap reference
 app.use('/css/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
-app.use(morgan('dev'));
+
+// Session support
+app.use(session({
+  resave: false, // Don't save session if unmodified
+  saveUninitialized: false, // Don't create session until something stored
+  secret: 'ChangeMe'
+}));
+
+// Parse request bodies (req.body)
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
-app.use(methodOverride());
 
-// Open listener
-app.listen(8080);
-console.log('App listening on port 8080');
+// Allow overriding methods in query (?_method=put)
+app.use(methodOverride('_method'));
+
+// Expose the "messages" local variable when views are rendered
+app.use(function(req, res, next) {
+  const msgs = req.session.messages || [];
+
+  // Expose "messages" local variable
+  res.locals.messages = msgs;
+
+  // Expose "hasMessages"
+  res.locals.hasMessages = !! msgs.length;
+
+  next();
+
+  // Empty or "flush" the messages so they don't build up
+  req.session.messages = [];
+})
+
+// Load controllers
+import boot from './controllers';
+boot(app, { verbose: !module.parent });
+
+app.use(function(err, req, res, next) {
+  // Log it
+  if(!module.parent) {
+    console.error(err.stack);
+  }
+
+  // Error page
+  res.status(500).render('5xx');
+});
+
+// Assume 404 since no middleware responded
+app.use(function(req, res, next) {
+  res.status(404).render('404', { url: req.originalUrl });
+})
+
+/* istanbull ignore next */
+if(!module.parent) {
+  // Open listener
+  app.listen(8080);
+  console.log('yara-online started on port 8080');
+}
