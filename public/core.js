2d543754759148fdffe96e6b5188646cc6d2e408
Thisita <Thisita@users.noreply.github.com>
2017-03-02 19:51:43 -0500

    [refactor] superceeded by app.js

--- public/core.js
+++
@@ -1,7 +0,0 @@
-import MainController from './controllers/MainController';
-import SearchService from './services/SearchService';
-
-angular
-  .module('app', [])
-  .controller('mainController', MainController)
-  .service('searchService', SearchService);
